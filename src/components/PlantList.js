3f2ed613e989a2a17e5562f6b9ee9cc79377afae
Quinn Lonergan <qui99lo@gmail.com>
2022-01-04 14:36:59 -0500

    removed unused code

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,27 +2,6 @@ import React, {useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({itemsToRender}) {
-  // const [plants, setPlants] = useState([])
-  // const [itemsToRender, setItemsToRender] = useState(plants)
-
-  // useEffect(() => { 
-  //   fetch("http://localhost:6001/plants")
-  //   .then((r)=>r.json())
-  //   .then((data) => setPlants(data))
-  // }, [plants])
-
-  // function makeItemsToRender(){
-  //   let searchedPlants = plants.filter((plant) => {
-  //     if (searchItem) return true;
-
-  //     return plant.name.includes(searchItem)
-      
-  //   })
-
-  //   console.log(searchedPlants)
-
-  //   setItemsToRender(searchedPlants)
-  // }
 
   
   return (
